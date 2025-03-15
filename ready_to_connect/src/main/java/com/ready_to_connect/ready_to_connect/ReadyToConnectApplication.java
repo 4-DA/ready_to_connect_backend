182d32afaa878146da47b16edf775e9dd8399850
villa <d.thomas71@yahoo.com>
2025-03-15 12:25:50 -0500

    added java project structure

---
+++ ready_to_connect/src/main/java/com/ready_to_connect/ready_to_connect/ReadyToConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ready_to_connect.ready_to_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyToConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadyToConnectApplication.class, args);
+	}
+
+}
---
+++ ready_to_connect/src/test/java/com/ready_to_connect/ready_to_connect/ReadyToConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ready_to_connect.ready_to_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyToConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
